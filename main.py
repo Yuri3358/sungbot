50ae9bd851fab03ee834e9141d75affa85f2b89d
Yuri3358 <yurialmeida917@gmail.com>
2023-07-22 11:46:34 -0300

    refactor: pycord && style: commets removed

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import discord
-from discord.ext import commands
-from os import listdir, system
+from os import listdir
 from accesskey import TOKEN
 
-bot = commands.Bot(">") #prefixo do bot
+bot = discord.Bot()
 
-for arquivo in listdir('./commands'): #obtêm os nomes de cada arquivo terminado em .py, excluindo a pasta '__pycache__'.
+for arquivo in listdir('./commands'): 
     if arquivo.endswith('.py'):
         bot.load_extension(f'commands.{arquivo[:-3]}')
 
-@bot.event #decorador de uma função que aguarda o evento (tentativa de login)
+@bot.event
 async def on_ready():
-    print(f'Login em {bot.user} efetuado com sucesso!') #mensagem que mostra o sucesso do login cliente-servidor, se aparecer isso, seu planejamento pode seguir normalmente
+    print(f'Login em {bot.user} efetuado com sucesso!')
 
-bot.run(TOKEN) #roda o bot, a constante token exige uma chave de acesso, recomendável em um arquivo modularizado
-
-system('cls') #limpa a linha de comando
+bot.run(TOKEN) 
