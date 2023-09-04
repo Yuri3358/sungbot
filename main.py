5923a6c5cdeb732f22117efafac0962ee8091e45
Yuri3358 <yurialmeida917@gmail.com>
2023-09-04 13:51:44 -0300

    refactor: error handler moved to sdm.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import discord
-from discord.ext import commands
 from os import listdir, environ
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,11 +14,4 @@ async def on_ready():
     discord.Activity(type=discord.ActivityType.listening, name="meus usuários")
     print(f'Logged as {bot.user}')
     
-@bot.event
-async def on_application_command_error(ctx, err):
-    if isinstance(err, commands.NotOwner):
-        await ctx.respond("Você não possui as permissões necessárias")
-    else:
-        raise err
-
 bot.run(environ["TOKEN"])
