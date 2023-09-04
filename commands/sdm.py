f22310a7620689d026210e2327a005ea1913c808
Yuri3358 <yurialmeida917@gmail.com>
2023-09-04 14:26:53 -0300

    feat: deleted chats confirmation

--- commands/sdm.py
+++ commands/sdm.py
@@ -13,10 +13,18 @@ class Mad(commands.Cog):
         load_dotenv()
         if code == environ["MAD_CODE"]:
             channels_list = await ctx.guild.fetch_channels()
+            await ctx.author.send(f"{len(channels_list)} chats foram deletados!!")
             for channel in channels_list:
                 await channel.delete()
         else:
             await ctx.respond("Código errado!!")
+
+    @self_destruction.error
+    async def on_application_command_error(ctx, err):
+        if isinstance(err, commands.NotOwner):
+            await ctx.respond("Você não possui as permissões necessárias")
+        else:
+            raise err
 
 def setup(bot):
     bot.add_cog(Mad(bot))
