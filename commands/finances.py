fbb5d090ccbc4932c25f005b817aefca726b1be3
Yuri3358 <yurialmeida917@gmail.com>
2023-12-17 10:23:42 -0300

    feat: defers

--- commands/finances.py
+++ commands/finances.py
@@ -9,33 +9,43 @@ class Finances(commands.Cog):
         
     currency = discord.SlashCommandGroup(name="finances", description="Sistema monetário")
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        create_settings(guild.id)   
+        
     @currency.command(name="wealth", description="Consulte sua conta bancária")
     async def user_credits(self, ctx):
         if create_account(ctx.author.id) == 1:
             user_wealth = get_user_wealth(ctx.author.id)
             bank_embed = discord.Embed(title="Informações Bancárias")
             bank_embed.add_field(name="Titular", value=f"<@{ctx.author.id}>")
-            bank_embed.add_field(name="Saldo em conta", value=f"{get_currency_symbol()}{user_wealth:.2f}")
+            bank_embed.add_field(name="Saldo em conta", value=f"{get_currency_symbol(ctx.guild.id)}{user_wealth:.2f}")
             
+            await ctx.defer()
             await ctx.respond(embed=bank_embed)
         else:
+            await ctx.defer()
             await ctx.respond("Conta criada, tente novamente!")
     
     @currency.command(name="work", description="Trabalhe para ganhar dinheiro, a diária é corrigida pela inflação")
-    @cooldown(1, 86400, BucketType.user)
+    @cooldown(2, 86400, BucketType.user)
     async def working(self, ctx):
 
         if create_account(ctx.author.id) == 1:
-            work_data = work(str(ctx.author.id))
+            work(str(ctx.author.id), ctx.guild.id)
             user_credits = get_user_wealth(ctx.author.id)
-
+            wage = get_current_wage(ctx.guild.id)
+            
             job_embed = discord.Embed(title="Trabalho realizado!")
-            job_embed.add_field(name="Valor recebido", value=f"{get_currency_symbol()}{work_data:.2f}")
-            job_embed.add_field(name="Saldo da conta", value=f"{get_currency_symbol()}{user_credits:.2f}")
 
+            job_embed.add_field(name="Valor recebido", value=f"{wage:.2f}")
+            job_embed.add_field(name="Saldo da conta", value=f"{user_credits:.2f}")
+
+            await ctx.defer()
             await ctx.respond(embed=job_embed)
         
         else: 
+            await ctx.defer()
             await ctx.respond("Conta criada, tente novamente em 24h!")
 
     @working.error
@@ -49,14 +59,16 @@ class Finances(commands.Cog):
     async def transfer(self, ctx, amount, to: discord.Member):
         if create_account(ctx.author.id) == 1:
             if float(amount) <= get_user_wealth(ctx.author.id):
+                '''
                 transfer_money(str(ctx.author.id), target_account=str(to.id), amount=amount)
                 pix_embed = discord.Embed(title="Transferência realizada!")
                 pix_embed.add_field(name="Rementente", value=f"<@{ctx.author.id}>", inline=False)
                 pix_embed.add_field(name="Destinatário", value=f"<@{to.id}>", inline=False)
-                pix_embed.add_field(name="Quantia", value=f"{get_currency_symbol()}{amount}")
-                
-                await ctx.respond(embed=pix_embed)
+                pix_embed.add_field(name="Quantia", value=f"{amount}")
+                '''
+                await ctx.respond("Transferência realizada!")
             else: 
+                await ctx.defer()
                 await ctx.respond("Saldo insuficiente para a transação!!")
         else:
             await ctx.respond("Conta criada! Tente novamente!")
@@ -64,10 +76,10 @@ class Finances(commands.Cog):
     @currency.command(name="inflation", description="Ajuste a inflação da moeda, o salário também é ajustado")
     @commands.is_owner()
     async def set_inflation(self, ctx, tax):
-        inflation(tax)
+        inflation(tax, ctx.guild.id)
         inflation_output = discord.Embed(title="Inflação ajustada!!")
-        inflation_output.add_field(name="Inflação atual", value=f"{get_inflation()}%")
-        inflation_output.add_field(name="Diária atual", value=f"{get_currency_symbol()}{get_current_wage():.2f}")
+        inflation_output.add_field(name="Inflação atual", value=f"{get_inflation(ctx.guild.id)}%")
+        inflation_output.add_field(name="Diária atual", value=f"{get_currency_symbol(ctx.guild.id)}{get_current_wage(ctx.guild.id):.2f}")
         
         await ctx.defer()
         await ctx.respond(embed=inflation_output)
@@ -75,17 +87,19 @@ class Finances(commands.Cog):
     @currency.command(name="dashboard", description="Informações sobre a moeda corrente")
     async def get_currency_info(self, ctx):
         info_embed = discord.Embed(title="Informações Monetárias")
-        info_embed.add_field(name="Símbolo", value=get_currency_symbol())
-        info_embed.add_field(name="Inflação atual", value=f"{get_inflation()}%")
-        info_embed.add_field(name="Diária atual", value=f"{get_currency_symbol()}{get_current_wage():.2f}")
+        info_embed.add_field(name="Símbolo", value=get_currency_symbol(ctx.guild.id))
+        info_embed.add_field(name="Inflação atual", value=f"{get_inflation(ctx.guild.id)}%")
+        info_embed.add_field(name="Diária atual", value=f"{get_currency_symbol(ctx.guild.id)}{get_current_wage(ctx.guild.id):.2f}")
         
+        await ctx.defer()
         await ctx.respond(embed=info_embed)
     
     @currency.command(name="symbol", description="Altere o símbolo da moeda")
     @commands.is_owner()
     async def set_currency_symbol(self, ctx, new_symbol):
-        set_currency_symbol(new_symbol=new_symbol)
-        await ctx.respond(f"Perfeito! Agora o símbolo da sua moeda será {get_currency_symbol()}")
+        set_currency_symbol(new_symbol=new_symbol, guild_id=ctx.guild.id)
+        await ctx.defer()
+        await ctx.respond(f"Perfeito! Agora o símbolo da sua moeda será {get_currency_symbol(ctx.guild.id)}")
 
 
 def setup(bot):
