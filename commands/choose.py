f950561d2d534a5a6dc1707acf449a49f61ce39d
Yuri3358 <yurialmeida917@gmail.com>
2023-12-27 18:09:25 -0300

    feat: choose options

---
+++ commands/choose.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from random import choice
+import discord
+
+class Chooser(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.slash_command(name="choose", description="Peça para eu escolher uma opção dentre várias! Separe entre vírgulas!")
+    async def chooser(self, ctx, elements):
+
+        options = elements.split(", ")
+        chose_option = choice(options)
+        await ctx.respond(f"Eu escolho {chose_option}!")
+
+def setup(bot):
+    bot.add_cog(Chooser(bot))
